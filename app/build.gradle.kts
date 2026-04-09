ee039acbd9868f35d2f9104fec9b9ff837a0503f
Francesco Miranda <framira2005@gmail.com>
2026-04-10 01:08:39 +0200

    Update project configuration and dependencies
    - Enable minification for release builds.
    - Upgrade Java and JVM target compatibility from version 11 to 17.
    - Add new dependencies for MapLibre GL Native, JTS Topology Suite, OkHttp, Retrofit, Moshi, and Ferrostar.
    - Update `libs.versions.toml` with the corresponding library versions and definitions.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -30,11 +30,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -42,6 +42,7 @@ android {
 }
 
 dependencies {
+    // Android & Compose
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -50,6 +51,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
+    // Test
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -57,4 +60,22 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    // MapLibre GL Native
+    implementation(libs.maplibre.android)
+
+    // JTS Topology Suite
+    implementation(libs.jts.core)
+
+    // OkHttp, Retrofit e Moshi (API OpenRouteService)
+    implementation(platform(libs.okhttp.bom))
+    implementation(libs.okhttp.core)
+    implementation(libs.okhttp.logging)
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.moshi)
+    implementation(libs.moshi.kotlin)
+
+    // Ferrostar
+    implementation(libs.ferrostar.core)
+    implementation(libs.ferrostar.maplibreui)
 }
